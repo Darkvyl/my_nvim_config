a2c0b8450bf937a70bad25806d2ef7c52f112339
Kirill Maneychik <kirill.maneychik@gmail.com>
2024-08-20 22:44:12 +0200

    Fix treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,11 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+ 
+	default = 
+	{
+		lazy = false,
+		version = nil
+	},
+	-- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c_sharp", "java", "lua", "vim", "vimdoc", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
