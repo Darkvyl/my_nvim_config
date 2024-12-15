f3e029e2db5273f4e1db4d6c33310dc6b2ed6b0d
Kirill Maneychik <kirill.maneychik@gmail.com>
2024-12-15 14:11:59 +0100

    updated remaps

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,5 @@
 require("config.remap")
 require("config.lazy")
 vim.wo.relativenumber = true
+
+
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Y", '"+yg_', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>yy", '"+yy', { noremap = true, silent = true })
+
+vim.keymap.set("v", "<leader>p", '"+p', { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>P", '"+P', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>p", '"+p', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>P", '"+P', { noremap = true, silent = true })
